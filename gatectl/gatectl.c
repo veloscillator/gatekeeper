956a4e924c682b52a90354fc62e7899e22e33ff2
Ken Kochis <kkochis@outlook.com>
2018-06-15 22:27:14 -0700

    Add user-mode gatectl.exe

---
+++ gatectl/gatectl.c
@@ -0,0 +1,34 @@
+/*++
+
+Module Name:
+
+	gatectl.c
+
+Abstract:
+
+	Application for interfacing with Gatekeeper filter driver. 
+
+Environment:
+
+	User mode
+
+--*/
+
+#include <stdlib.h>
+
+int _cdecl
+main(
+	_In_ int argc,
+	_In_reads_(argc) char* argv[])
+/*++
+
+Routine Description:
+
+	Entry point.
+
+--*/
+{
+	(void)argc;
+	(void)argv;
+	return 0;
+}
